0f5e40a512b6a5faf294241539e920786b295a75
pandias1 <smartsuresh.p84@gmail.com>
2016-11-02 12:26:59 -0400

    git push request added jenkins

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Jenkin Demo project!!!" );
-        System.out.println( "Welcome to Jenkin Demo" );
         System.out.println( "Welcome to Jenkin integrate with github" );
     }
 }
