eebd9768bd6442fab87ce4928ec9818200a98b10
pandias1 <smartsuresh.p84@gmail.com>
2016-11-02 11:16:09 -0400

    Added new line to app.java file

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Jenkin Demo project!!!" );
+        System.out.println( "Welcome to Jenkin Demo" );
     }
 }
