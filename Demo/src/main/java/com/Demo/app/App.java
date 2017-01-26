06f3342a8ff2148fe0b957d8aab2bd4ba7d966c0
pandias1 <smartsuresh.p84@gmail.com>
2017-01-26 16:27:48 -0500

    Release1.1

--- Demo/src/main/java/com/Demo/app/App.java
+++ Demo/src/main/java/com/Demo/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Demo project!!" );
+	System.out.println("This is for Release1.1 tag version")
     }
 }
